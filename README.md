e6606ef01be6640085e35d4070c42ad633be1c7e
Wanna Sleep <88297290+wantZzz@users.noreply.github.com>
2024-05-13 19:08:11 +0800

    initial upload

---
+++ README.md
@@ -0,0 +1,16 @@
+# Only play first
+<p align="center">一個可以右鍵單獨播放youtube播放清單中的第一個影片的瀏覽器擴展</p>
+<p align="center">A browser extension that can right-click to play the first video in the YouTube playlist</p>
+
+## 如何使用 | How to use
+打開任一 `https://www.youtube.com/` 開頭網頁，左鍵撥放清單即可看見 `只播放第一部影片` 選項
+![image](https://github.com/wantZzz/Only_play_first/blob/main/rightclick.png)
+
+## 安裝 | install
+
+> 由於 Chrome 的資安政策不允許使用者自行安裝未列在 Chrome 線上應用程式商店 中，故請以 __載入未封裝項目__ 方式安裝本插件
+
+1. 開啟 Chrome 管理擴充功能
+2. 開啟 開發人員模式
+3. 載入未封裝項目並選取解壓縮完的資料夾
+4. Have Fun!
---
+++ background.js
@@ -0,0 +1,19 @@
+chrome.contextMenus.onClicked.addListener(function (info, tab) {
+	switch (info.menuItemId) {
+		case 'YOPF_YoutubeUrlGet':
+			const clicked_url = new URL(info.linkUrl);
+			if (clicked_url.host == "www.youtube.com" && clicked_url.searchParams.has('v')){
+				const frist_voido_url = "https://www.youtube.com/watch?v=" + clicked_url.searchParams.get('v');
+				chrome.tabs.create({url: frist_voido_url, openerTabId: tab.openerTabId});
+			}
+	}
+});
+
+chrome.runtime.onInstalled.addListener(function (details){
+	chrome.contextMenus.create({  
+        id: 'YOPF_YoutubeUrlGet',
+        type: 'normal',
+        title: '只播放第一部影片',
+        contexts: ['link']
+    });
+});
